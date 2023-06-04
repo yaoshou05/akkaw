746cb2313dcb96ca40a1d060c8f1cc2a7a8341ed
yaoshou05 <114859195+yaoshou05@users.noreply.github.com>
2023-06-04 19:01:24 +0800

    akkaw
    akkaw

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using baler_aurora_layout.Services;
+using baler_aurora_layout.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace baler_aurora_layout
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using baler_aurora_layout.ViewModels;
+using baler_aurora_layout.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace baler_aurora_layout
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
